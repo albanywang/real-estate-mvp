d6b1e0830520dd767f661861fe402592b9b6c8d2
albanywang <--global>
2025-07-27 02:40:21 -0400

    add some sample data

--- client/src/components/LoginPopup.js
+++ client/src/components/LoginPopup.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../services/authContext';
+import { useAuth } from '../services/AuthContext';
 
 // Login Popup Component - Integrated with User Database
 const LoginPopup = ({ isOpen, onClose }) => {
