75b631670367fad28f40758893fe23c3a5871c40
albanywang <--global>
2025-07-27 10:28:14 -0400

    add login function

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,10 @@ const rootElement = document.getElementById('root');
 
 if (rootElement) {
   const root = createRoot(rootElement);
-  root.render(<App />);
+  root.render(  
+    <AuthProvider>
+      <App />
+    </AuthProvider>);
 } else {
   console.error('Root element not found');
 }
